ac00af8c5d9be3b3f954f8bcc61118503fefb0e5
Evan Shipley-courter <evanshipley-courter@Evans-MacBook-Pro.local>
2026-05-16 12:08:37 -0400

    Add setup.py for global pip distribution support

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jarvis-dev",
+    version="0.1.4",
+    packages=find_packages(),
+    py_modules=['cli'],
+    install_requires=[
+        "typer", "rich", "requests", "watchdog", "sounddevice", "scipy", 
+        "numpy", "sentence-transformers", "faiss-cpu", "SpeechRecognition", 
+        "prompt_toolkit", "dropbox", "google-api-python-client", "google-auth-oauthlib",
+        "paramiko", "psutil", "Pillow"
+    ],
+    entry_points={
+        'console_scripts': [
+            'jarvis=cli:app',
+        ],
+    },
+)
